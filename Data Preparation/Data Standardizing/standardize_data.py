6693a03d812ad792b0a0a5db5ed6367bd44a55da
TrueMohsen <seaoceania4@email.com>
2023-03-11 20:55:59 +0330

    Add data standardizing file

---
+++ Data Preparation/Data Standardizing/standardize_data.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 14:57:41 2023
+
+@author: mohsenshojaeiyeganeh@gmail.com
+"""
+
+from pandas import read_csv
+import numpy
+from sklearn.preprocessing import StandardScaler
+from numpy import set_printoptions
+
+#To read data_set
+file_name = "../../Data_Set/pima-indians-diabetes.csv"
+column_names = ['preg','plas','pres','skin','test','mass','pedi','age','class']
+data_set = read_csv(file_name, names=column_names)
+
+#Converting Data frame into an array
+data_in_array = data_set.values
+
+#Separating data_array into input and output arrays
+input_array = data_in_array[:,0:8]
+output_array = data_in_array[:,8]
+
+scaler = StandardScaler().fit(input_array)
+rescaled_input_array = scaler.transform(input_array)
+
+set_printoptions(precision=3)
+print(input_array)
+print("Rescaled input is: ")
+print(rescaled_input_array)
+
