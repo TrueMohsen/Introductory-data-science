d46f9f5b37b11788df07a7067529f8d7a570b9d1
TrueMohsen <seaoceania4@email.com>
2023-03-21 02:27:38 +0330

    add train_test_split evaluation algorithm

--- 3-Algorithm Evaluation/train_test Split/train_test_Split.py
+++ 3-Algorithm Evaluation/train_test Split/train_test_Split.py
@@ -6,8 +6,8 @@ Created on Fri Mar 10 14:57:41 2023
 """
 
 from pandas import read_csv
-import numpy
-from sklearn.ensemble import ExtraTreesClassifier 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 #To read data_set
 file_name = "../../Data_Set/pima-indians-diabetes.csv"
@@ -21,8 +21,12 @@ data_in_array = data_set.values
 input_array = data_in_array[:,0:8]
 output_array = data_in_array[:,8]
 
+test_size = 0.33
+seed = 6
+X_train,X_test,Y_train,Y_test = train_test_split(input_array,output_array,test_size=test_size,random_state=seed)
+model = LogisticRegression(solver='liblinear')
+model.fit(X_train,Y_train)
 
-model = ExtraTreesClassifier(n_estimators=100)
-model.fit(input_array,output_array)
-#this prints a number for each feature, the bigger the number the more important it is.
-print(model.feature_importances_)
+result = model.score(X_test,Y_test)
+print("Accuracy: %.3f%%" %(result*100.0)) 
+ 
