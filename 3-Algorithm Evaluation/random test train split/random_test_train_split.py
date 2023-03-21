bde1c2577e1930ea82ad7c09acd7151f32fb647f
TrueMohsen <seaoceania4@email.com>
2023-03-21 21:53:53 +0330

    add random test-train split

---
+++ 3-Algorithm Evaluation/random test train split/random_test_train_split.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 14:57:41 2023
+
+@author: mohsenshojaeiyeganeh@gmail.com
+"""
+
+from pandas import read_csv
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+#To read data_set
+file_name = "../../Data_Set/pima-indians-diabetes.csv"
+column_names = ['preg','plas','pres','skin','test','mass','pedi','age','class']
+data_set = read_csv(file_name, names=column_names)
+
+#Converting Data frame into an array
+data_in_array = data_set.values
+
+#Separating data_array into input and output arrays
+input_array = data_in_array[:,0:8]
+output_array = data_in_array[:,8]
+
+number_of_splits = 10
+test_size = 0.33 
+seed = 7 
+
+evaluation = ShuffleSplit(n_splits = number_of_splits,test_size = test_size, random_state= seed)
+model = LogisticRegression(solver = "liblinear")
+
+results = cross_val_score(model, input_array,output_array,cv = evaluation)
+print("Accuracy: %.3f%% (%.3f%%)" %(results.mean()*100.0,results.std()*100.0)) 
+
